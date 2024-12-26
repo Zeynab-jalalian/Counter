1eb3f322a1551a9fdef29272919d5da4e22871c4
zeynab jalalian <zjalalianweb@gmail.com>
2024-12-26 11:19:08 +0330

    js code and fix bug

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ let positive=document.getElementById('positive');
 let negative=document.getElementById('negative');
 let number=document.getElementById('number');
 let count=0;
+positive.addEventListener('click' , function(){
+    count++;
+    update();
+})
+negative.addEventListener('click' , function(){
+    count--;
+    update();
+})
+function update(){
+    number.innerHTML=count;
+}
